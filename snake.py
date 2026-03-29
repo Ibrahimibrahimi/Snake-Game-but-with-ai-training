43d346d877bb09230785ad85cc05e0e0dfdc50f9
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-29 16:35:57 +0100

    remove the grid from the game

--- snake.py
+++ snake.py
@@ -2,7 +2,7 @@ import math
 import random
 import pygame
 import random
-from model.environnement import snake , cube
+from model.environnement import snake, cube
 
 width = 500
 height = 500
@@ -11,11 +11,9 @@ cols = 25
 rows = 20
 
 
-
-
 def redrawWindow():
     global win
-    win.fill((0,0,0))
+    win.fill((0, 0, 0))
     # drawGrid(width, rows, win)
     s.draw(win)
     snack.draw(win)
@@ -23,44 +21,29 @@ def redrawWindow():
     pass
 
 
-
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
-
-    x = 0
-    y = 0
-    for l in range(rows):
-        x = x + sizeBtwn
-        y = y +sizeBtwn
-
-        pygame.draw.line(surface, (255,255,255), (x, 0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0, y),(w,y))
-    
-
-
 def randomSnack(rows, item):
     positions = item.body
 
     while True:
-        x = random.randrange(1,rows-1)
-        y = random.randrange(1,rows-1)
-        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
-               continue
+        x = random.randrange(1, rows-1)
+        y = random.randrange(1, rows-1)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
         else:
-               break
+            break
 
-    return (x,y)
+    return (x, y)
 
 
 def main():
     global s, snack, win
-    win = pygame.display.set_mode((width,height))
-    s = snake((255,0,0), (10,10))
+    win = pygame.display.set_mode((width, height))
+    s = snake((255, 0, 0), (10, 10))
     s.addCube()
-    snack = cube(randomSnack(rows,s), color=(0,255,0))
+    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
     flag = True
     clock = pygame.time.Clock()
-    
+
     while flag:
         pygame.time.delay(50)
         clock.tick(10)
@@ -72,14 +55,15 @@ def main():
 
         if s.body[0].pos == snack.pos:
             s.addCube()
-            snack = cube(randomSnack(rows,s), color=(0,255,0))
-            
+            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
         for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x+1:])):
                 print("Score:", len(s.body))
-                s.reset((10,10))
+                s.reset((10, 10))
                 break
-                    
+
         redrawWindow()
+
 
 main()
